693fbcd2f915586533ad61baa8871751656ddbfc
konstantin0s <constantintofan85@gmail.com>
2019-02-06 21:04:42 +0100

    Add base pizza with no toppings & show them on click

--- pizza.js
+++ pizza.js
@@ -1,4 +1,9 @@
 // Write your Pizza Builder JavaScript in this file.
+$(".green-pepper").each(function(i){
+
+  //For Each Instance, grab a child called .something1. Fade It Out.
+  $(this).fadeOut(0);
+});
 
 $('.btn-pepperonni').on('click', function(e) {
   $('.green-pepper').toggle();  
@@ -6,16 +11,32 @@ $('.btn-pepperonni').on('click', function(e) {
 
 });
 
+
+$(".mushroom").each(function(i){
+
+  //For Each Instance, grab a child called .something1. Fade It Out.
+  $(this).fadeOut(0);
+});
 $('.btn-mushrooms').on('click', function(e) {
   $('.mushroom').toggle(); 
   e.preventDefault();
 });
+
+//display none pepperoni.
+$(".pep").each(function(i){
+
+  //For Each Instance, grab a child called .something1. Fade It Out.
+  $(this).fadeOut(0);
+});
+
 
 $('.btn-green-peppers').on('click', function(e) {
   $('.pep').toggle(); 
   e.preventDefault();
 });
 
+$('.sauce').css('display', 'none');
+// $("#id").css("display", "block");
 $('.btn-sauce').on('click', function(e) {
   $('.sauce').toggle(); 
   e.preventDefault();
