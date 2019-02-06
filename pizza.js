822dc36d98429c4fe28dd29f2af66c8287201d43
konstantin0s <constantintofan85@gmail.com>
2019-02-06 21:33:39 +0100

    Add change button class active based on it's current state

--- pizza.js
+++ pizza.js
@@ -1,29 +1,32 @@
 // Write your Pizza Builder JavaScript in this file.
-$(".green-pepper").each(function(i){
-
-  //For Each Instance, grab a child called .something1. Fade It Out.
+$('.green-pepper').each(function(i){
   $(this).fadeOut(0);
 });
 
 $('.btn-pepperonni').on('click', function(e) {
-  $('.green-pepper').toggle();  
+  
   e.preventDefault();
 
+  if ($('#pepperoni').hasClass('active')) {
+    $('#pepperoni').removeClass('active');  
+   } else {
+     ($('#pepperoni').addClass('active'));
+   }
+     $('.green-pepper').toggle(); 
 });
 
 
-$(".mushroom").each(function(i){
-
-  //For Each Instance, grab a child called .something1. Fade It Out.
+$('.mushroom').each(function(i){
   $(this).fadeOut(0);
 });
+
 $('.btn-mushrooms').on('click', function(e) {
   $('.mushroom').toggle(); 
   e.preventDefault();
 });
 
 //display none pepperoni.
-$(".pep").each(function(i){
+$('.pep').each(function(i){
 
   //For Each Instance, grab a child called .something1. Fade It Out.
   $(this).fadeOut(0);
@@ -31,12 +34,12 @@ $(".pep").each(function(i){
 
 
 $('.btn-green-peppers').on('click', function(e) {
+
   $('.pep').toggle(); 
   e.preventDefault();
 });
 
 $('.sauce').css('display', 'none');
-// $("#id").css("display", "block");
 $('.btn-sauce').on('click', function(e) {
   $('.sauce').toggle(); 
   e.preventDefault();
@@ -47,10 +50,18 @@ $('#crust').removeClass('crust-gluten-free');
 $('.btn-crust').on('click', function(e) {
 
  e.preventDefault();
- if ($( "#crust" ).hasClass('crust-gluten-free')) {
-  $( "#crust" ).removeClass('crust-gluten-free');
+ if ($('#crust' ).hasClass('crust-gluten-free')) {
+  $( '#crust' ).removeClass('crust-gluten-free');
  } else {
-  $( "#crust" ).addClass('crust-gluten-free'); 
+  $( '#crust' ).addClass('crust-gluten-free'); 
  }
   });
+
+  //Remove active class from all buttons
+  $( window ).load(function() {
+    $('#pepperoni').removeClass('active');
+  });
+
+ 
+
 
