e0805456f15c117dbffae0b6879cf113b8993c63
konstantin0s <constantintofan85@gmail.com>
2019-02-06 14:28:06 +0100

    Add toggle on click button

--- pizza.js
+++ pizza.js
@@ -1,1 +1,28 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$('.btn-pepperonni').on('click', function(e) {
+  $('.green-pepper').toggle(); //you can list several class names 
+  e.preventDefault();
+
+});
+
+$('.btn-mushrooms').on('click', function(e) {
+  $('.mushroom').toggle(); //you can list several class names 
+  e.preventDefault();
+});
+
+$('.btn-green-peppers').on('click', function(e) {
+  $('.pep').toggle(); //you can list several class names 
+  e.preventDefault();
+});
+
+$('.btn-sauce').on('click', function(e) {
+  $('.sauce').toggle(); //you can list several class names 
+  e.preventDefault();
+});
+
+
+$('.btn-crust').on('click', function(e) {
+  $('.crust-gluten-free').toggle(); //you can list several class names 
+  e.preventDefault();
+});
